cb1bc2b6510f444b32246bd1230db63bfc79b962
Moosa Ali <moosaali@macbookair.home>
2025-12-12 09:00:46 -0500

    setting up environment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+const PORT = process.env.PORT || 3000;
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+  console.log(`Server is running on http://localhost:${PORT}`)
 })
