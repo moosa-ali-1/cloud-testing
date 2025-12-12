1f29b466d175697dc7286569ed24c4124d8663bf
Moosa Ali <moosaali@macbookair.home>
2025-12-12 17:25:39 -0500

    Adding health probe endpoint

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+// Health check endpoint
+app.get('/healthz', (req, res) => {
+    res.status(200).send('Healthy');
+});
+
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
